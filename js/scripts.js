830df4d215a1b4b258c837fc038c95547ce6952b
Roberto Larivera <robylariv@gmail.com>
2022-12-21 15:50:53 +0100

    ciclo tab * 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 console.log('int ok');  //inizializzazione progetto
-for (let i = 1 ; i <= 100 ; i++){
+for (let i = 1 ; i <= 10 ; i++){
     console.log('i',i,typeof i);
-    
+    let tabbellina = i * 3;
+    console.log('tabbellina',tabbellina,typeof tabbellina);
 }
