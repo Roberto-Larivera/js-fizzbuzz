5aa38b56570481542f8fef561cfae2daff819480
Roberto Larivera <robylariv@gmail.com>
2022-12-21 18:07:14 +0100

    Aggiunta di commenti e completato esercizio base + stampa in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,35 @@ console.log('int ok');  //inizializzazione progetto
 for (let i = 1 ; i <= 15 ; i++){
     // console.log('i',i,typeof i); //debug
 
-    const ul = document.getElementById("box-ul");
-    console.log('ul', ul, typeof ul);
-    const li = document.createElement('li');
-    ul.append(li);
+    const ul = document.getElementById("box-ul"); //prendo un 'elemento ul' del DOM tramite il suo id
+    //console.log('ul', ul, typeof ul); //debug
+    const li = document.createElement('li');    //creo un elemento del DOM
+    ul.append(li);  //aggiungo elemento appena creato nel 'ul' precedente
 
     if (i % 5 == 0 && i % 3 == 0){
-        //console.log('sono un multiplo di tre e cinque',i); //debug
-        console.log('FizzBuzz');
-        li.innerHTML = 'FizzBuzz';
+        //console.log('sono un divisibile di tre e cinque',i); //debug
+       
+        console.log('FizzBuzz');    //stampa console
+        li.innerHTML = 'FizzBuzz';  //stampa dom
     }
     else if (i % 3 == 0){
-        //console.log('sono un multiplo di tre',i); //debug
-        console.log('Fizz');
-        li.innerHTML = 'Fizz';
+        //console.log('sono un divisibile di tre',i); //debug
+        console.log('Fizz');    //stampa console
+        li.innerHTML = 'Fizz';  //stampa dom
 
     }
     else if (i % 5 == 0){
-        //console.log('sono un multiplo di cinque',i); //debug
-        console.log('Buzz');
-        li.innerHTML = 'Buzz';
+        //console.log('sono un divisibile di cinque',i); //debug
+        
+        console.log('Buzz');    //stampa console
+        li.innerHTML = 'Buzz';  //stampa dom
 
     }
     else{
-        //console.log('non sono un divisore', i); //debug
-        console.log(i)
-        li.innerHTML = i;
-
+        //console.log('tutti i numeri che non sono divisibili per 3 e 5', i); //debug
+        
+        console.log(i)    //stampa console
+        li.innerHTML = i;  //stampa dom
     }
 
 
@@ -38,49 +40,3 @@ for (let i = 1 ; i <= 15 ; i++){
     //newLi.className = `box box-${index}`;
 
 }
-
-
-
-
-
-
-
-
-
-// for (let i = 1 ; i <= 10 ; i++){
-//     // console.log('i',i,typeof i);
-//     let tabUno = i * 1;
-//     console.log(tabUno, i);
-//     let tabbellinaTre = i * 3;
-//     // console.log('tabbellina',tabbellina,typeof tabbellina);
-//     console.log(tabbellinaTre, 'Fizz');
-//     let tabbellinaCinque = i * 5;
-//     console.log(tabbellinaCinque, 'Buzz');
-// }
-// let numeroQuasiCasuale = 98764;
-// // Verificare se numeroQuasiCasuale è pari. Se è pari, alertare "Pari", altrimenti alertare "Dispari"
-// if (numeroQuasiCasuale % 2 == 0) {
-//     alert("Pari");
-// }
-// else {
-//     alert("Dispari");
-// }
-
-
-
-// console.log('PRIMA DEL CICLO FOR');
-
-// for (let i = 0; i < 10; i++) {
-
-//     console.log('DURANTE IL CICLO FOR', i);
-
-//     document.getElementById('mio-div').innerHTML += '<p>' + (i + 1) + '° P</p>';
-
-// } 
-
-// console.log('DOPO IL CICLO FOR');
-
-// let risultatoDivisione = (10 / 3);
-// let risultatoModulo = (10 % 3);
-// console.log('risultatoDivisione 10 / 3: ' + risultatoDivisione);
-// console.log('risultatoModulo 10 % 3: ' + risultatoModulo);
