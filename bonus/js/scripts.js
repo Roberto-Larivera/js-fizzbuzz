8eacfe48fc22f5d89a16ebb024dfd90517a295ab
Roberto Larivera <robylariv@gmail.com>
2022-12-22 11:08:44 +0100

    Aggiunta di messaggio

--- bonus/js/scripts.js
+++ bonus/js/scripts.js
@@ -10,37 +10,50 @@ for (let i = 1 ; i <= 100 ; i++){
     box.className = "box";  //bonus
     //console.log(box.classList); //debug
 
+
+    let messaggio;
     if (i % 5 == 0 && i % 3 == 0){  
         //console.log('sono un divisibile di tre e cinque',i); //debug
         console.log('FizzBuzz');    //stampa console
-        box.innerHTML = 'FizzBuzz';  //stampa dom  //bonus
+        //box.innerHTML = 'FizzBuzz';  //stampa dom  //bonus   //messaggio
         box.classList.add("fizz-buzz");  //bonus
         //console.log(box.classList);
+
+        messaggio = "FizzBuzz";
     }
     else if (i % 3 == 0){
         //console.log('sono un divisibile di tre',i); //debug
         console.log('Fizz');    //stampa console
-        box.innerHTML = 'Fizz';  //stampa dom  //bonus
+        //box.innerHTML = 'Fizz';  //stampa dom  //bonus   //messaggio
         box.classList.add("fizz");  //bonus
         //console.log(box.classList); //debug
+
+        messaggio = "Fizz";
+
     }
     else if (i % 5 == 0){
         //console.log('sono un divisibile di cinque',i); //debug
         console.log('Buzz');    //stampa console
-        box.innerHTML = 'Buzz';  //stampa dom  //bonus
+        //box.innerHTML = 'Buzz';  //stampa dom  //bonus   //messaggio
         box.classList.add("buzz");  //bonus
         //console.log(box.classList); //debug
+
+        messaggio = "Buzz";
+
 
     }
     else{
         //console.log('tutti i numeri che non sono divisibili per 3 e 5', i); //debug
         console.log(i)    //stampa console
-        box.innerHTML = i;  //stampa dom  //bonus
+        //box.innerHTML = i;  //stampa dom  //bonus   //messaggio
 
         box.classList.add("default-box");  //bonus
         //console.log(box.classList); //debug
-    }
 
+        messaggio = i;
+
+    }
+    box.innerHTML = messaggio;
 
     // ul.append(li);
     // li.append(i);
