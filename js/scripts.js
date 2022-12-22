5c2d155161d5661d74fd0fea1fb6fedc5a08f4dd
Roberto Larivera <robylariv@gmail.com>
2022-12-22 05:51:38 +0100

    Completato Esercizio Principale

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,43 @@
-console.log('int ok');  //inizializzazione progetto
+//console.log('int ok');  //inizializzazione progetto
 for (let i = 1 ; i <= 100 ; i++){
     // console.log('i',i,typeof i); //debug
 
-    const containerBox = document.getElementById("container-box"); //prendo un 'elemento ul' del DOM tramite il suo id
+    const containerBox = document.getElementById("container-box"); //prendo un 'elemento ul' del DOM tramite il suo id    //bonus
     //console.log('ul', ul, typeof ul); //debug
-    const b = document.createElement('div');
-    const box = document.createElement('div');    //creo un elemento del DOM
-    containerBox.append(box);
-    box.className = "box";
+    //const b = document.createElement('div');  //bonus
+    //const box = document.createElement('div');    //creo un elemento del DOM  //bonus
+   //containerBox.append(box);     //bonus
+    //box.className = "box";  //bonus
     //console.log(box.classList); //debug
 
-    if (i % 5 == 0 && i % 3 == 0){
+    if (i % 5 == 0 && i % 3 == 0){  
         //console.log('sono un divisibile di tre e cinque',i); //debug
         console.log('FizzBuzz');    //stampa console
-        box.innerHTML = 'FizzBuzz';  //stampa dom
-        box.classList.add("fizz-buzz");
+        //box.innerHTML = 'FizzBuzz';  //stampa dom  //bonus
+        //box.classList.add("fizz-buzz");  //bonus
         //console.log(box.classList);
     }
     else if (i % 3 == 0){
         //console.log('sono un divisibile di tre',i); //debug
         console.log('Fizz');    //stampa console
-        box.innerHTML = 'Fizz';  //stampa dom
-        box.classList.add("fizz");
+        //box.innerHTML = 'Fizz';  //stampa dom  //bonus
+        //box.classList.add("fizz");  //bonus
         //console.log(box.classList); //debug
     }
     else if (i % 5 == 0){
         //console.log('sono un divisibile di cinque',i); //debug
         console.log('Buzz');    //stampa console
-        box.innerHTML = 'Buzz';  //stampa dom
-        box.classList.add("buzz");
+        //box.innerHTML = 'Buzz';  //stampa dom  //bonus
+        //box.classList.add("buzz");  //bonus
         //console.log(box.classList); //debug
 
     }
     else{
         //console.log('tutti i numeri che non sono divisibili per 3 e 5', i); //debug
         console.log(i)    //stampa console
-        box.innerHTML = i;  //stampa dom
+        //box.innerHTML = i;  //stampa dom  //bonus
 
-        box.classList.add("default-box");
+        //box.classList.add("default-box");  //bonus
         //console.log(box.classList); //debug
     }
 
